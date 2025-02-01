4e54e1971a041bcc68b24111807dfa3cafe378a4
unname <lwer210@naver.com>
2025-02-02 01:14:17 +0900

    feature: Auth API 컨트롤러 추가

---
+++ src/main/java/com/example/board/auth/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.board.auth.controller;
+
+import com.example.board.auth.controller.request.LoginRequest;
+import com.example.board.auth.controller.request.RegisterRequest;
+import com.example.board.auth.controller.response.LoginResponse;
+import com.example.board.auth.controller.response.RegisterResponse;
+import com.example.board.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
