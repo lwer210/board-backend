d4ea27d9ece0fc9ab2693deab5bd81f601dc40ff
unname <lwer210@naver.com>
2025-02-02 01:12:39 +0900

    feature: 네이밍 메소드 추가

--- src/main/java/com/example/board/auth/persistence/repository/UserEntityRepository.java
+++ src/main/java/com/example/board/auth/persistence/repository/UserEntityRepository.java
@@ -8,4 +8,8 @@ import java.util.Optional;
 public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
