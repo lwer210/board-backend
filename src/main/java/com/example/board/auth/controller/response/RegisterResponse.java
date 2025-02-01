ff3ab3254be6945a25065178550965a36df23300
unname <lwer210@naver.com>
2025-02-02 01:15:28 +0900

    feature: 회원가입 API 응답 클래스

---
+++ src/main/java/com/example/board/auth/controller/response/RegisterResponse.java
@@ -0,0 +1,27 @@
+package com.example.board.auth.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterResponse {
+
+    private Long id;
+
+    private String email;
+
+    private String nickname;
+
+    private String role;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
