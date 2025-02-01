0c5a92025331225c16eaa68c71c2d44c9ffda0ca
unname <lwer210@naver.com>
2025-02-01 23:56:21 +0900

    feature: 사용자 Entity 추가

---
+++ src/main/java/com/example/board/auth/persistence/entity/UserEntity.java
@@ -0,0 +1,40 @@
+package com.example.board.auth.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "user_info")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class UserEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_SEQ")
+    @Comment("사용자 시퀀스")
+    private Long id;
+
+    @Column(name = "email", length = 150, nullable = false)
+    @Comment("사용자 이메일")
+    private String email;
+
+    @Column(name = "nickname", length = 100, nullable = false)
+    @Comment("사용자 닉네임")
+    private String nickname;
+
+    @Column(name = "password", length = 200, nullable = false)
+    @Comment("사용자 비밀번호")
+    private String password;
+
+    @Column(name = "role", length = 20, nullable = false)
+    @Comment("사용자 권한")
+    private String role; // TODO 추후 Enum 클래스로 변경 필요
+
+    // TODO 게시글 Entity 연관관계 설정 필요
+}
