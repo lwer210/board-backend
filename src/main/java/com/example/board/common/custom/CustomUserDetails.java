46cf92f70622d968ee96d90fac4edc8b66758d07
unname <lwer210@naver.com>
2025-02-01 23:57:30 +0900

    feature: UserDetails 추가

---
+++ src/main/java/com/example/board/common/custom/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.example.board.common.custom;
+
+import com.example.board.auth.persistence.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final UserEntity userEntity;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(this.userEntity.getRole().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userEntity.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
