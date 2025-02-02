d4e17460e66cf0e04b60de76a22426012d5571b7
unname <lwer210@naver.com>
2025-02-02 20:20:14 +0900

    feature: 로그인 로직 Transactional 추가

--- src/main/java/com/example/board/auth/service/AuthService.java
+++ src/main/java/com/example/board/auth/service/AuthService.java
@@ -18,6 +18,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -61,6 +62,7 @@ public class AuthService {
                 .build();
     }
 
+    @Transactional
     public LoginResponse login(LoginRequest request) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
