7d1729245f6aa5477f4573bda947294fde0d80fc
unname <lwer210@naver.com>
2025-02-05 02:20:54 +0900

    feature: 메일 API Controller 추가

---
+++ src/main/java/com/example/board/auth/controller/MailController.java
@@ -0,0 +1,16 @@
+package com.example.board.auth.controller;
+
+import com.example.board.auth.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/mail")
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+
+}
