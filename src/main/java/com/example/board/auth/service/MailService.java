d7bde74023cab92c667ac861616a9a3f1a25ead9
unname <lwer210@naver.com>
2025-02-05 02:21:21 +0900

    feature: 메일 API Service 추가

---
+++ src/main/java/com/example/board/auth/service/MailService.java
@@ -0,0 +1,10 @@
+package com.example.board.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+}
