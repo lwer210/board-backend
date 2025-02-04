53e890b3e1d804a8eb0d2da0e6e5480d4bb8ddda
unname <lwer210@naver.com>
2025-02-05 02:12:57 +0900

    feature: 토큰 재발급 API 추가

--- src/main/java/com/example/board/auth/controller/AuthController.java
+++ src/main/java/com/example/board/auth/controller/AuthController.java
@@ -1,10 +1,18 @@
 package com.example.board.auth.controller;
 
 import com.example.board.auth.controller.request.LoginRequest;
+import com.example.board.auth.controller.request.RefreshRequest;
 import com.example.board.auth.controller.request.RegisterRequest;
 import com.example.board.auth.controller.response.LoginResponse;
 import com.example.board.auth.controller.response.RegisterResponse;
 import com.example.board.auth.service.AuthService;
+import com.example.board.common.jwt.JwtDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,17 +23,62 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth API Controller")
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
+    @Operation(summary = "사용자 회원가입 API", description = "사용자 회원가입 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = RegisterResponse.class))
+            }, description = "회원가입 성공 시 반환")
+    })
+    public ResponseEntity<RegisterResponse> register(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    description = "사용자 회원가입 요청 객체",
+                    content = @Content(schema = @Schema(implementation = RegisterRequest.class))
+            )
+            @RequestBody RegisterRequest request
+    ) {
         return ResponseEntity.ok(authService.register(request));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+    @Operation(summary = "사용자 로그인 API", description = "사용자 로그인 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponse.class))
+            }, description = "로그인 성공 시 반환")
+    })
+    public ResponseEntity<LoginResponse> login(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    description = ("로그인 요청 객체"),
+                    content = @Content(schema = @Schema(implementation = LoginRequest.class))
+            )
+            @RequestBody LoginRequest request
+    ) {
         return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/refresh")
+    @Operation(summary = "토큰 재발급 API", description = "refresh 토큰을 사용한 jwt 토큰 재발급 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponse.class))
+            }, description = "재발급 성공 시 반환")
+    })
+    public ResponseEntity<LoginResponse> refresh(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    description = "토큰 재발급 요청 객체",
+                    content = @Content(schema = @Schema(implementation = RefreshRequest.class))
+            )
+            @RequestBody RefreshRequest request
+    ) {
+        return ResponseEntity.ok(authService.refresh(request));
     }
 }
