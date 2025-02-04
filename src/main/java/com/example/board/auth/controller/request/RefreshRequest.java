775d64bc96c528879a0ca9b5fbaaee70d64941bc
unname <lwer210@naver.com>
2025-02-05 02:12:39 +0900

    feature: 토큰 재발급 요청 객체 추가

---
+++ src/main/java/com/example/board/auth/controller/request/RefreshRequest.java
@@ -0,0 +1,13 @@
+package com.example.board.auth.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshRequest {
+
+    private String refreshToken;
+}
