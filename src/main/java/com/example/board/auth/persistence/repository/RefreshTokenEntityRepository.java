ca49082fde06efed44da13c13d10dcdcab7cb2c1
unname <lwer210@naver.com>
2025-02-05 02:13:12 +0900

    feature: findByToken 메소드 추가

--- src/main/java/com/example/board/auth/persistence/repository/RefreshTokenEntityRepository.java
+++ src/main/java/com/example/board/auth/persistence/repository/RefreshTokenEntityRepository.java
@@ -3,8 +3,12 @@ package com.example.board.auth.persistence.repository;
 import com.example.board.auth.persistence.entity.RefreshTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenEntityRepository extends JpaRepository<RefreshTokenEntity, Long> {
     boolean existsByUserId(Long userId);
 
     void deleteByUserId(Long userId);
+
+    Optional<RefreshTokenEntity> findByToken(String token);
 }
