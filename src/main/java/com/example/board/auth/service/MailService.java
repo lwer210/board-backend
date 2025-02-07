c6fbe8b9c5aa5e4eae1e930ec623cc62e70ca709
unname <lwer210@naver.com>
2025-02-08 01:19:06 +0900

    feature: 이메일 발송 API 비즈니스 로직

--- src/main/java/com/example/board/auth/service/MailService.java
+++ src/main/java/com/example/board/auth/service/MailService.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @Service
