c81d918c75deb01f3105d21c9f68e5de219caf41
unname <lwer210@naver.com>
2025-02-08 01:04:47 +0900

    feature: 응답 및 swagger 명세 추가

--- src/main/java/com/example/board/auth/controller/MailController.java
+++ src/main/java/com/example/board/auth/controller/MailController.java
@@ -1,7 +1,14 @@
 package com.example.board.auth.controller;
 
 import com.example.board.auth.controller.request.PasswordResetRequest;
+import com.example.board.auth.controller.response.UserResponse;
 import com.example.board.auth.service.MailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/mail")
 @RequiredArgsConstructor
+@Tag(name = "이메일 발송 API Controller", description = "이메일 발송 API Controller")
 public class MailController {
 
     private final MailService mailService;
 
+    @Operation(summary = "임시 비밀번호 발급 API", description = "임시 비밀번호를 발급 후 이메일을 발송")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))
+            }, description = "성공 시 반환")
+    })
     @PostMapping("/password/reset")
-    public ResponseEntity<?> reset(@RequestBody PasswordResetRequest request){
-        mailService.reset(request);
-        return null;
+    public ResponseEntity<UserResponse> reset(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    description = "임시 비밀번호 발급 요청 객체",
+                    content = @Content(schema = @Schema(implementation = PasswordResetRequest.class))
+            )
+            @RequestBody PasswordResetRequest request
+    ){
+        return ResponseEntity.ok(mailService.reset(request));
     }
 }
