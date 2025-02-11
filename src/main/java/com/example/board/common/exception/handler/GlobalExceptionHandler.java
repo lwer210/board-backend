77a404ed6d0235c18a48cc5afc3e79efce266653
unname <lwer210@naver.com>
2025-02-11 21:09:42 +0900

    feature: 전역 Exception 핸들러 추가

---
+++ src/main/java/com/example/board/common/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,89 @@
+package com.example.board.common.exception.handler;
+
+import com.example.board.common.exception.*;
+import com.example.board.common.exception.response.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {
+            NewPasswordNotMatchesException.class
+    })
+    public ResponseEntity<ExceptionResponse> handleNewPasswordNotMatchesException(NewPasswordNotMatchesException e) {
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {
+            AlreadyRegisteredEmailException.class,
+            AlreadyRegisteredNicknameException.class
+    })
+    public ResponseEntity<ExceptionResponse> alreadyExceptionHandler(Exception e){
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(HttpStatus.CONFLICT.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(value = {
+            UserNotFoundException.class,
+            TokenNotFoundException.class
+    })
+    public ResponseEntity<ExceptionResponse> userNotFoundExceptionHandler(Exception e){
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(HttpStatus.NOT_FOUND.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {
+            EmailSendFailedException.class,
+            ParseTokenException.class
+    })
+    public ResponseEntity<ExceptionResponse> emailSendFailedExceptionHandler(Exception e){
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {
+            ExpiredTokenException.class,
+            NotContainsRoleException.class,
+            UnauthorizedException.class
+    })
+    public ResponseEntity<ExceptionResponse> expiredTokenExceptionHandler(Exception e){
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(HttpStatus.UNAUTHORIZED.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(value = {
+            Exception.class
+    })
+    public ResponseEntity<ExceptionResponse> exceptionHandler(Exception e){
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
