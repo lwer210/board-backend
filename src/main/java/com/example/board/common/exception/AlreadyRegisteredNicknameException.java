a30a4e5f6d6b4076df380cb8f4fb9f6cf6c35a61
unname <lwer210@naver.com>
2025-02-11 21:08:57 +0900

    fix: 이름 수정

--- src/main/java/com/example/board/common/exception/AlreadyRegisteredNicknameException.java
+++ src/main/java/com/example/board/common/exception/AlreadyRegisteredNicknameException.java
@@ -3,19 +3,19 @@ package com.example.board.common.exception;
 import lombok.Getter;
 
 @Getter
-public class AlreadyRegisteredNickname extends RuntimeException {
+public class AlreadyRegisteredNicknameException extends RuntimeException {
 
     private static final String DEFAULT_MESSAGE = "이미 가입된 닉네임입니다.";
 
-    public AlreadyRegisteredNickname() {
+    public AlreadyRegisteredNicknameException() {
         super(DEFAULT_MESSAGE);
     }
 
-    public AlreadyRegisteredNickname(String message) {
+    public AlreadyRegisteredNicknameException(String message) {
         super(message);
     }
 
-    public AlreadyRegisteredNickname(String message, Throwable cause) {
+    public AlreadyRegisteredNicknameException(String message, Throwable cause) {
         super(message, cause);
     }
 }
