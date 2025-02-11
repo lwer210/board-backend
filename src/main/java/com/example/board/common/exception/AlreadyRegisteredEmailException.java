e54c5337195f60953dddfe6f0eff495c1abc4e1d
unname <lwer210@naver.com>
2025-02-11 21:08:36 +0900

    feature: Email 발송 커스텀 예외 추가

--- src/main/java/com/example/board/common/exception/AlreadyRegisteredEmailException.java
+++ src/main/java/com/example/board/common/exception/AlreadyRegisteredEmailException.java
@@ -1,22 +1,21 @@
 package com.example.board.common.exception;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
-public class AlreadyRegisteredEmail extends RuntimeException{
+public class AlreadyRegisteredEmailException extends RuntimeException{
 
     private static final String DEFAULT_MESSAGE = "이미 가입된 이메일입니다.";
 
-    public AlreadyRegisteredEmail() {
+    public AlreadyRegisteredEmailException() {
         super(DEFAULT_MESSAGE);
     }
 
-    public AlreadyRegisteredEmail(String message) {
+    public AlreadyRegisteredEmailException(String message) {
         super(message);
     }
 
-    public AlreadyRegisteredEmail(String message, Throwable cause) {
+    public AlreadyRegisteredEmailException(String message, Throwable cause) {
         super(message, cause);
     }
 }
