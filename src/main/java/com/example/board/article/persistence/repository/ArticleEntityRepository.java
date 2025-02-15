0d058652498833893b6e494095f36bdff4ac85ac
unname <lwer210@naver.com>
2025-02-15 13:22:27 +0900

    feature: findAllByPublicYn 메소드 추가

--- src/main/java/com/example/board/article/persistence/repository/ArticleEntityRepository.java
+++ src/main/java/com/example/board/article/persistence/repository/ArticleEntityRepository.java
@@ -1,7 +1,10 @@
 package com.example.board.article.persistence.repository;
 
 import com.example.board.article.persistence.entity.ArticleEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleEntityRepository extends JpaRepository<ArticleEntity, Long> {
+    Page<ArticleEntity> findAllByPublicYn(Pageable pageable, String publicYn);
 }
