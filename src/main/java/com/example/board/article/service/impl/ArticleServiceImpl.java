13512f0113fc1528599a014fcd9cba3eb5390b87
unname <lwer210@naver.com>
2025-02-15 13:22:42 +0900

    feature: list, all 메소드 구현 로직 추가

--- src/main/java/com/example/board/article/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/example/board/article/service/impl/ArticleServiceImpl.java
@@ -2,23 +2,57 @@ package com.example.board.article.service.impl;
 
 import com.example.board.article.controller.request.ArticleRequest;
 import com.example.board.article.controller.response.ArticleResponse;
+import com.example.board.article.persistence.entity.ArticleEntity;
 import com.example.board.article.persistence.repository.ArticleEntityRepository;
 import com.example.board.article.service.ArticleService;
+import com.example.board.auth.persistence.entity.UserEntity;
+import com.example.board.auth.persistence.repository.UserEntityRepository;
 import com.example.board.common.custom.CustomUserDetails;
+import com.example.board.common.exception.UnauthorizedException;
+import com.example.board.common.exception.UserNotFoundException;
+import com.example.board.common.paging.Pagination;
 import com.example.board.common.paging.PagingResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleServiceImpl implements ArticleService {
 
     private final ArticleEntityRepository articleEntityRepository;
+    private final UserEntityRepository userEntityRepository;
 
     @Override
     public PagingResponse<ArticleResponse> list(Pageable pageable) {
-        return null;
+        Page<ArticleEntity> page = articleEntityRepository.findAllByPublicYn(pageable, "Y");
+
+        Pagination pagination = Pagination.builder()
+                .size(page.getSize())
+                .numberOfElement(page.getNumberOfElements())
+                .totalElement(page.getTotalElements())
+                .totalPage(page.getTotalPages())
+                .number(page.getNumber())
+                .build();
+
+        List<ArticleEntity> list = page.getContent();
+
+        List<ArticleResponse> result = list.stream().map(value -> {
+            return ArticleResponse.builder()
+                    .articleId(value.getId())
+                    .title(value.getTitle())
+                    .content(value.getContent())
+                    .publicYn(value.getPublicYn())
+                    .build();
+        }).toList();
+
+        return PagingResponse.<ArticleResponse>builder()
+                .content(result)
+                .pagination(pagination)
+                .build();
     }
 
     @Override
@@ -33,7 +67,28 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public ArticleResponse add(CustomUserDetails userDetails, ArticleRequest request) {
-        return null;
+        if(userDetails == null){
+            throw new UnauthorizedException();
+        }
+
+        UserEntity user = userEntityRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(UserNotFoundException::new);
+
+        ArticleEntity entity = ArticleEntity.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .publicYn(request.getPublicYn())
+                .user(user)
+                .build();
+
+        articleEntityRepository.save(entity);
+
+        return ArticleResponse.builder()
+                .articleId(entity.getId())
+                .title(request.getTitle())
+                .content(request.getContent())
+                .publicYn(request.getPublicYn())
+                .build();
     }
 
     @Override
