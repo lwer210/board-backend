cd4025312a2fa087edc47381a30f7c7e38088258
unname <lwer210@naver.com>
2025-02-16 11:04:18 +0900

    feature: 게시글 API 서비스 인터페이스 추가

--- src/main/java/com/example/board/article/service/ArticleService.java
+++ src/main/java/com/example/board/article/service/ArticleService.java
@@ -1,7 +1,9 @@
 package com.example.board.article.service;
 
 import com.example.board.article.controller.request.ArticleRequest;
+import com.example.board.article.controller.request.UpdateArticleRequest;
 import com.example.board.article.controller.response.ArticleResponse;
+import com.example.board.article.controller.response.DeleteArticleResponse;
 import com.example.board.common.custom.CustomUserDetails;
 import com.example.board.common.paging.PagingResponse;
 import org.springframework.data.domain.Pageable;
@@ -10,13 +12,13 @@ public interface ArticleService {
 
     public PagingResponse<ArticleResponse> list(Pageable pageable);
 
-    public PagingResponse<ArticleResponse> getUserArticleList(CustomUserDetails userDetails);
+    public PagingResponse<ArticleResponse> getUserArticleList(CustomUserDetails userDetails, Pageable pageable);
 
     public ArticleResponse info(Long articleId);
 
     public ArticleResponse add(CustomUserDetails userDetails, ArticleRequest request);
 
-    public ArticleResponse update(CustomUserDetails userDetails, ArticleRequest request);
+    public ArticleResponse update(CustomUserDetails userDetails, UpdateArticleRequest request);
 
-    public ArticleResponse delete(Long articleId);
+    public DeleteArticleResponse delete(Long articleId);
 }
