d4b5d21f963d4c5c33d1401e4c6803f6edfdad6e
unname <lwer210@naver.com>
2025-02-16 11:23:57 +0900

    feature: findByIdAndPublicYn 네이밍 메소드 추가

--- src/main/java/com/example/board/article/persistence/repository/ArticleEntityRepository.java
+++ src/main/java/com/example/board/article/persistence/repository/ArticleEntityRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArticleEntityRepository extends JpaRepository<ArticleEntity, Long> {
     Page<ArticleEntity> findAllByPublicYn(Pageable pageable, String publicYn);
 
     Page<ArticleEntity> findAllByUserIdAndPublicYn(Long userId, String publicYn, Pageable pageable);
+
+    Optional<ArticleEntity> findByIdAndPublicYn(Long id, String publicYn);
 }
