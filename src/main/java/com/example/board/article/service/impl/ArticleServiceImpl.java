ae142a4429c8b1f726d842bfedb838c38fe66c1b
unname <lwer210@naver.com>
2025-02-16 11:04:38 +0900

    feature: 게시글 인터페이스 구현체 추가

--- src/main/java/com/example/board/article/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/example/board/article/service/impl/ArticleServiceImpl.java
@@ -1,13 +1,16 @@
 package com.example.board.article.service.impl;
 
 import com.example.board.article.controller.request.ArticleRequest;
+import com.example.board.article.controller.request.UpdateArticleRequest;
 import com.example.board.article.controller.response.ArticleResponse;
+import com.example.board.article.controller.response.DeleteArticleResponse;
 import com.example.board.article.persistence.entity.ArticleEntity;
 import com.example.board.article.persistence.repository.ArticleEntityRepository;
 import com.example.board.article.service.ArticleService;
 import com.example.board.auth.persistence.entity.UserEntity;
 import com.example.board.auth.persistence.repository.UserEntityRepository;
 import com.example.board.common.custom.CustomUserDetails;
+import com.example.board.common.exception.ArticleNotFoundException;
 import com.example.board.common.exception.UnauthorizedException;
 import com.example.board.common.exception.UserNotFoundException;
 import com.example.board.common.paging.Pagination;
@@ -56,13 +59,48 @@ public class ArticleServiceImpl implements ArticleService {
     }
 
     @Override
-    public PagingResponse<ArticleResponse> getUserArticleList(CustomUserDetails userDetails) {
-        return null;
+    public PagingResponse<ArticleResponse> getUserArticleList(CustomUserDetails userDetails, Pageable pageable) {
+        UserEntity user = userEntityRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(UserNotFoundException::new);
+
+        Page<ArticleEntity> page = articleEntityRepository.
+                findAllByUserIdAndPublicYn(user.getId(), "Y", pageable);
+
+        Pagination pagination = Pagination.builder()
+                .size(page.getSize())
+                .numberOfElement(page.getNumberOfElements())
+                .totalElement(page.getTotalElements())
+                .totalPage(page.getTotalPages())
+                .number(page.getNumber())
+                .build();
+
+        List<ArticleResponse> list = page.getContent().stream()
+                .map(value -> {
+                    return ArticleResponse.builder()
+                            .articleId(value.getId())
+                            .title(value.getTitle())
+                            .content(value.getContent())
+                            .publicYn(value.getPublicYn())
+                            .build();
+                }).toList();
+
+        return PagingResponse.<ArticleResponse>builder()
+                .pagination(pagination)
+                .content(list)
+                .build();
     }
 
     @Override
     public ArticleResponse info(Long articleId) {
-        return null;
+        ArticleEntity articleEntity = articleEntityRepository.findById(articleId)
+                .orElseThrow(ArticleNotFoundException::new);
+
+        return ArticleResponse.builder()
+                .articleId(articleEntity.getId())
+                .title(articleEntity.getTitle())
+                .content(articleEntity.getContent())
+                .publicYn(articleEntity.getPublicYn())
+                .build();
     }
 
     @Override
@@ -92,12 +130,43 @@ public class ArticleServiceImpl implements ArticleService {
     }
 
     @Override
-    public ArticleResponse update(CustomUserDetails userDetails, ArticleRequest request) {
-        return null;
+    public ArticleResponse update(CustomUserDetails userDetails, UpdateArticleRequest request) {
+        if(userDetails == null){
+            throw new UnauthorizedException();
+        }
+
+        ArticleEntity article = articleEntityRepository.findById(request.getArticleId())
+                .orElseThrow(ArticleNotFoundException::new);
+
+        if(request.getContent() != null){
+            article.setContent(request.getContent());
+        }
+        if(request.getTitle() != null){
+            article.setTitle(request.getTitle());
+        }
+        if(request.getPublicYn() != null){
+            article.setPublicYn(request.getPublicYn());
+        }
+
+        ArticleEntity save = articleEntityRepository.save(article);
+
+        return ArticleResponse.builder()
+                .articleId(save.getId())
+                .title(save.getTitle())
+                .content(save.getContent())
+                .publicYn(save.getPublicYn())
+                .build();
     }
 
     @Override
-    public ArticleResponse delete(Long articleId) {
-        return null;
+    public DeleteArticleResponse delete(Long articleId) {
+        ArticleEntity articleEntity = articleEntityRepository.findById(articleId)
+                .orElseThrow(ArticleNotFoundException::new);
+
+        articleEntityRepository.delete(articleEntity);
+
+        return DeleteArticleResponse.builder()
+                .isDelete(true)
+                .build();
     }
 }
