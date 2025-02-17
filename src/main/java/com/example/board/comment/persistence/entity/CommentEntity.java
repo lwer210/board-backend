572e4369d7f21f9e4ec89aaf73d4c044b76a570d
unname <lwer210@naver.com>
2025-02-17 22:14:49 +0900

    feature: 댓글 Entity 추가

---
+++ src/main/java/com/example/board/comment/persistence/entity/CommentEntity.java
@@ -0,0 +1,35 @@
+package com.example.board.comment.persistence.entity;
+
+import com.example.board.article.persistence.entity.ArticleEntity;
+import com.example.board.auth.persistence.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "comment_info")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_seq")
+    @Comment("댓글 시퀀스")
+    private Long id;
+
+    @Column(name = "answer", length = 500)
+    @Comment("댓글")
+    private String answer;
+
+    @Column(name = "like")
+    @Comment("좋아요 수")
+    private Long like;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_seq", nullable = false)
+    private ArticleEntity article;
+}
