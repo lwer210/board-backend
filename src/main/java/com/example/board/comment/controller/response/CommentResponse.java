267c671626e46e1f54f9ecf2e0e35562740e903a
unname <lwer210@naver.com>
2025-02-17 22:15:31 +0900

    feature: 댓글 응답 객체 추가

---
+++ src/main/java/com/example/board/comment/controller/response/CommentResponse.java
@@ -0,0 +1,23 @@
+package com.example.board.comment.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentResponse {
+
+    private Long commentId;
+
+    private String answer;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
