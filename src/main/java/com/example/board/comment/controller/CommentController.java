23598df038fcf8b308a3bd54576c814f623eb042
unname <lwer210@naver.com>
2025-02-18 21:25:55 +0900

    feature: 댓글 추가 API 추가

--- src/main/java/com/example/board/comment/controller/CommentController.java
+++ src/main/java/com/example/board/comment/controller/CommentController.java
@@ -1,9 +1,10 @@
 package com.example.board.comment.controller;
 
+import com.example.board.comment.controller.request.CommentRequest;
 import com.example.board.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/comment")
@@ -11,4 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController {
 
     private final CommentService commentService;
+
+    @PostMapping("/add/{articleSeq}")
+    public ResponseEntity<?> add(
+            @PathVariable Long articleSeq,
+            @RequestBody CommentRequest request
+            ){
+        return ResponseEntity.ok(commentService.add(articleSeq, request));
+    }
 }
