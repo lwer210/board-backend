f120c5183404d9dbec1210ccbdd6bac93362bea6
unname <lwer210@naver.com>
2025-02-18 21:18:29 +0900

    feature: 댓글 추가 메소드 구현 추가

--- src/main/java/com/example/board/comment/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/board/comment/service/impl/CommentServiceImpl.java
@@ -1,8 +1,13 @@
 package com.example.board.comment.service.impl;
 
+import com.example.board.article.persistence.entity.ArticleEntity;
+import com.example.board.article.persistence.repository.ArticleEntityRepository;
 import com.example.board.comment.controller.request.CommentRequest;
+import com.example.board.comment.controller.response.CommentResponse;
+import com.example.board.comment.persistence.entity.CommentEntity;
 import com.example.board.comment.persistence.repository.CommentEntityRepository;
 import com.example.board.comment.service.CommentService;
+import com.example.board.common.exception.ArticleNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,10 +17,28 @@ import org.springframework.stereotype.Service;
 public class CommentServiceImpl implements CommentService {
 
     private final CommentEntityRepository commentEntityRepository;
+    private final ArticleEntityRepository articleEntityRepository;
 
     @Override
-    public void add(Long articleSeq, CommentRequest request) {
+    public CommentResponse add(Long articleSeq, CommentRequest request) {
+        ArticleEntity articleEntity = articleEntityRepository.findById(articleSeq)
+                .orElseThrow(ArticleNotFoundException::new);
 
+        CommentEntity comment = CommentEntity.builder()
+                .answer(request.getAnswer())
+                .like(0L)
+                .article(articleEntity)
+                .build();
+
+        CommentEntity save = commentEntityRepository.save(comment);
+
+        return CommentResponse.builder()
+                .commentId(save.getId())
+                .answer(save.getAnswer())
+                .like(save.getLike())
+                .createdAt(save.getCreatedAt())
+                .updatedAt(save.getUpdatedAt())
+                .build();
     }
 
     @Override
