5f5d03e4a195873742edb6c2f0493cf285e762e1
lwer210 <lwer210@naver.com>
2025-02-19 11:40:19 +0900

    feature: 댓글 삭제 응답 객체 추가

---
+++ src/main/java/com/example/board/comment/controller/response/DeleteCommentResponse.java
@@ -0,0 +1,15 @@
+package com.example.board.comment.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DeleteCommentResponse {
+
+    private Boolean isDelete;
+}
