d2e0935cc22dea42029143369ccea418cc62b07e
lwer210 <lwer210@naver.com>
2025-02-19 10:17:27 +0900

    feature: 댓글 추가 api 토큰 검증 추가

--- src/main/java/com/example/board/comment/controller/CommentController.java
+++ src/main/java/com/example/board/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.example.board.comment.controller;
 import com.example.board.comment.controller.request.CommentRequest;
 import com.example.board.comment.controller.response.CommentResponse;
 import com.example.board.comment.service.CommentService;
+import com.example.board.common.custom.CustomUserDetails;
 import com.example.board.common.exception.response.ExceptionResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -10,9 +11,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -32,7 +36,9 @@ public class CommentController {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))
             })
     })
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping("/add/{articleSeq}")
+    @SecurityRequirement(name = "Authentication Bearer")
     public ResponseEntity<CommentResponse> add(
             @Parameter(name = "articleSeq", required = true, description = "게시글 seq")
             @PathVariable Long articleSeq,
@@ -41,8 +47,9 @@ public class CommentController {
                     description = "댓글 추가 요청 객체",
                     content = @Content(schema = @Schema(implementation = CommentRequest.class))
             )
-            @RequestBody CommentRequest request
+            @RequestBody CommentRequest request,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
             ){
-        return ResponseEntity.ok(commentService.add(articleSeq, request));
+        return ResponseEntity.ok(commentService.add(articleSeq, request, customUserDetails));
     }
 }
