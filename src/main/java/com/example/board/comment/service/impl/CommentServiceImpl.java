6aeea418cc681a424c569e14a41713c24d746b0e
lwer210 <lwer210@naver.com>
2025-02-19 10:19:46 +0900

    fix: 댓글 추가 메소드 구현 수정

--- src/main/java/com/example/board/comment/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/board/comment/service/impl/CommentServiceImpl.java
@@ -2,12 +2,17 @@ package com.example.board.comment.service.impl;
 
 import com.example.board.article.persistence.entity.ArticleEntity;
 import com.example.board.article.persistence.repository.ArticleEntityRepository;
+import com.example.board.auth.persistence.entity.UserEntity;
+import com.example.board.auth.persistence.repository.UserEntityRepository;
 import com.example.board.comment.controller.request.CommentRequest;
 import com.example.board.comment.controller.response.CommentResponse;
 import com.example.board.comment.persistence.entity.CommentEntity;
 import com.example.board.comment.persistence.repository.CommentEntityRepository;
 import com.example.board.comment.service.CommentService;
+import com.example.board.common.custom.CustomUserDetails;
 import com.example.board.common.exception.ArticleNotFoundException;
+import com.example.board.common.exception.UnauthorizedException;
+import com.example.board.common.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -18,22 +23,32 @@ public class CommentServiceImpl implements CommentService {
 
     private final CommentEntityRepository commentEntityRepository;
     private final ArticleEntityRepository articleEntityRepository;
+    private final UserEntityRepository userEntityRepository;
 
     @Override
-    public CommentResponse add(Long articleSeq, CommentRequest request) {
-        ArticleEntity articleEntity = articleEntityRepository.findById(articleSeq)
+    public CommentResponse add(Long articleSeq, CommentRequest request, CustomUserDetails customUserDetails) {
+        if(customUserDetails == null){
+            throw new UnauthorizedException();
+        }
+
+        UserEntity user = userEntityRepository.findByEmail(customUserDetails.getUsername())
+                .orElseThrow(UserNotFoundException::new);
+
+        ArticleEntity articleEntity = articleEntityRepository.findByIdAndPublicYn(articleSeq, "Y")
                 .orElseThrow(ArticleNotFoundException::new);
 
         CommentEntity comment = CommentEntity.builder()
                 .answer(request.getAnswer())
                 .like(0L)
                 .article(articleEntity)
+                .userSeq(user.getId())
                 .build();
 
         CommentEntity save = commentEntityRepository.save(comment);
 
         return CommentResponse.builder()
                 .commentId(save.getId())
+                .nickname(user.getNickname())
                 .answer(save.getAnswer())
                 .like(save.getLike())
                 .createdAt(save.getCreatedAt())
@@ -47,17 +62,17 @@ public class CommentServiceImpl implements CommentService {
     }
 
     @Override
-    public void update(Long commentSeq, CommentRequest request) {
+    public void update(Long commentSeq, CommentRequest request, CustomUserDetails customUserDetails) {
 
     }
 
     @Override
-    public void delete(Long commentSeq) {
+    public void delete(Long commentSeq, CustomUserDetails customUserDetails) {
 
     }
 
     @Override
-    public void countList(Long commentSeq) {
+    public void countAdd(Long commentSeq, CustomUserDetails customUserDetails) {
 
     }
 }
