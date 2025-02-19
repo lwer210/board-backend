f7e2ec42c7ea80b80e7b81472b18e47ce053f2e1
lwer210 <lwer210@naver.com>
2025-02-19 11:39:36 +0900

    fix: 메소드 매개변수, 반환 타입 수정

--- src/main/java/com/example/board/comment/service/CommentService.java
+++ src/main/java/com/example/board/comment/service/CommentService.java
@@ -2,18 +2,20 @@ package com.example.board.comment.service;
 
 import com.example.board.comment.controller.request.CommentRequest;
 import com.example.board.comment.controller.response.CommentResponse;
+import com.example.board.comment.controller.response.DeleteCommentResponse;
 import com.example.board.common.custom.CustomUserDetails;
+import com.example.board.common.paging.PagingResponse;
 import org.springframework.data.domain.Pageable;
 
 public interface CommentService {
 
     public CommentResponse add(Long articleSeq, CommentRequest request, CustomUserDetails customUserDetails);
 
-    public void list(Long articleSeq, Pageable pageable);
+    public PagingResponse<CommentResponse> list(Long articleSeq, Pageable pageable, CustomUserDetails customUserDetails);
 
-    public void update(Long commentSeq, CommentRequest request, CustomUserDetails customUserDetails);
+    public CommentResponse update(Long commentSeq, CommentRequest request, CustomUserDetails customUserDetails);
 
-    public void delete(Long commentSeq, CustomUserDetails customUserDetails);
+    public DeleteCommentResponse delete(Long commentSeq, CustomUserDetails customUserDetails);
 
     public void countAdd(Long commentSeq, CustomUserDetails customUserDetails);
 }
