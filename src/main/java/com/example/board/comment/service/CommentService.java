bb14a2d2c588f1f3e6123328fc2456fd9e383ae6
lwer210 <lwer210@naver.com>
2025-02-19 10:18:47 +0900

    feature: 메소드 매개 변수 추가

--- src/main/java/com/example/board/comment/service/CommentService.java
+++ src/main/java/com/example/board/comment/service/CommentService.java
@@ -2,17 +2,18 @@ package com.example.board.comment.service;
 
 import com.example.board.comment.controller.request.CommentRequest;
 import com.example.board.comment.controller.response.CommentResponse;
+import com.example.board.common.custom.CustomUserDetails;
 import org.springframework.data.domain.Pageable;
 
 public interface CommentService {
 
-    public CommentResponse add(Long articleSeq, CommentRequest request);
+    public CommentResponse add(Long articleSeq, CommentRequest request, CustomUserDetails customUserDetails);
 
     public void list(Long articleSeq, Pageable pageable);
 
-    public void update(Long commentSeq, CommentRequest request);
+    public void update(Long commentSeq, CommentRequest request, CustomUserDetails customUserDetails);
 
-    public void delete(Long commentSeq);
+    public void delete(Long commentSeq, CustomUserDetails customUserDetails);
 
-    public void countList(Long commentSeq);
+    public void countAdd(Long commentSeq, CustomUserDetails customUserDetails);
 }
