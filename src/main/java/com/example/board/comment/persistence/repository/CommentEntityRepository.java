85ca4850d6c2e9fbec49ebee6d29f3fb4875aa0d
lwer210 <lwer210@naver.com>
2025-02-19 11:38:53 +0900

    feature: 네이밍 메소드 추가

--- src/main/java/com/example/board/comment/persistence/repository/CommentEntityRepository.java
+++ src/main/java/com/example/board/comment/persistence/repository/CommentEntityRepository.java
@@ -1,7 +1,10 @@
 package com.example.board.comment.persistence.repository;
 
 import com.example.board.comment.persistence.entity.CommentEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Long> {
+    Page<CommentEntity> findAllByArticle_Id(Long articleId, Pageable pageable);
 }
