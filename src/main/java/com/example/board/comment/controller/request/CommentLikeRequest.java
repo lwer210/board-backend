ad98de1e7fe64bf233215f547a445ecbb5ef04cd
lwer210 <lwer210@naver.com>
2025-02-20 21:44:50 +0900

    feature: 댓글 좋아요 요청 객체

---
+++ src/main/java/com/example/board/comment/controller/request/CommentLikeRequest.java
@@ -0,0 +1,13 @@
+package com.example.board.comment.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentLikeRequest {
+
+    private Long commentSeq;
+}
