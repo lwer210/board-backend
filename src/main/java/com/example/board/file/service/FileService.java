3727c36a20ce49e5ef9e42b337a5fc3696f6bdb8
lwer210 <lwer210@naver.com>
2025-02-21 22:15:46 +0900

    fix: 기타 메소드 삭제

--- src/main/java/com/example/board/file/service/FileService.java
+++ src/main/java/com/example/board/file/service/FileService.java
@@ -1,11 +1,12 @@
 package com.example.board.file.service;
 
+import com.example.board.common.custom.CustomUserDetails;
 import com.example.board.file.controller.response.FileResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
-    public FileResponse upload(MultipartFile file);
+    public FileResponse upload(MultipartFile file, CustomUserDetails customUserDetails);
 
     public void download();
 }
