9a6820acfd136a23ad9f08012723f4468104db5c
lwer210 <lwer210@naver.com>
2025-02-21 21:36:00 +0900

    fix: Default message 수정

--- src/main/java/com/example/board/common/exception/CommentNotFoundException.java
+++ src/main/java/com/example/board/common/exception/CommentNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.board.common.exception;
 
 public class CommentNotFoundException extends RuntimeException {
 
-    private static final String DEFAULT_MESSAGE = "Comment not found";
+    private static final String DEFAULT_MESSAGE = "댓글을 찾을 수 없습니다.";
 
     public CommentNotFoundException() {
         super(DEFAULT_MESSAGE);
