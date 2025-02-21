53b5c3daa64b71d281d7dad7b58e81f7569e1025
lwer210 <lwer210@naver.com>
2025-02-21 21:37:47 +0900

    feature: 파일 업로드 API 추가

--- src/main/java/com/example/board/file/controller/FileController.java
+++ src/main/java/com/example/board/file/controller/FileController.java
@@ -1,13 +1,27 @@
 package com.example.board.file.controller;
 
+import com.example.board.file.controller.response.FileResponse;
+import com.example.board.file.service.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/file")
 public class FileController {
 
+    private final FileService fileService;
 
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<FileResponse> upload(
+            @RequestParam(name = "file") MultipartFile file
+            ){
+        return ResponseEntity.ok(fileService.upload(file));
+    }
 }
